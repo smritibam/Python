2a735e45887cd9a6a2703a4ccc3c6209b42b4822
smritibam <127776309+smritibam@users.noreply.github.com>
2026-01-30 12:13:46 +0545

    Python comments,Escape sequence character

---
+++ begining Code/pythonDay02.py
@@ -0,0 +1,25 @@
+#comment in Python
+#Single line comment
+print("i am good and this is a good day")
+print("hello world") #printing
+
+#Multiline comment
+'''
+print("helloworld")
+print("for multiple comment we can use triple single Quote")
+print("Or we can also use tripl double quote")
+'''
+#escape sequence characters
+# #esto character jun chai directly string ma insert garna sakinna we use escape sequence character
+print("hey i am a \"good\" girl")
+print("hey you\'re doing ok")
+print("hi my name is smriti \n its good tohave you\n\n")#new line \n
+
+#print statement
+print("hey",6 ,7)
+print("hey","you",6,7,sep="~")#hey~you~6~7
+print("hey","you",6,7,sep="-")#hey-you-6-7
+print("\n\n")
+
+#end parameter specify what to print at the end
+print("hey","you",6,7,sep="~",end="009\n")#hey~you~6~7009
